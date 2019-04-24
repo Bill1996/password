d93868ae38663d458343d01885965dc48604029f
Ssiswent <liyugu971208@gmail.com>
2019-04-24 10:07:22 +0800

    新增FirstLoginFragment

--- app/src/main/java/vvfgaa/password/ui/MainActivity.java
+++ app/src/main/java/vvfgaa/password/ui/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import vvfgaa.password.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new FirstLoginFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
         if (savedInstanceState == null) {
-            KeyListFragment fragment = new KeyListFragment();
+            KeyListFragment fragment1 = new KeyListFragment();
 
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, fragment, KeyListFragment.TAG).commit();
+                    .add(R.id.fragment_container, fragment1, KeyListFragment.TAG).commit();
         }
     }
 }
