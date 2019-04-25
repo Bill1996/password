57ca8afe7f044b0b6c33d7954ebacaee908e94eb
Ssiswent <liyugu971208@gmail.com>
2019-04-25 16:07:53 +0800

    登录或注册成功后销毁当前Fragment

--- app/src/main/java/vvfgaa/password/ui/MainActivity.java
+++ app/src/main/java/vvfgaa/password/ui/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     public void loginToListFragment()
     {
         switchFragment(listFragment).commit();
-
+        getActivity().onBackPressed();
     }
 
     @Override
